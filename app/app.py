eb989ec60d610a0320bfb4273e9de9ef638b5c52
kurokuroB <h.ogawatja@gmail.com>
2023-06-26 18:55:05 +0900

    update: format

--- app/app.py
+++ app/app.py
@@ -5,16 +5,17 @@ from langchain.chains import ConversationChain
 
 app = Flask(__name__)
 
-#グローバル変数
+# グローバル変数
 MODEL = "text-davinci-003"
 
 # 会話用のインスタンスconversationを作成
-llm = OpenAI(model_name=MODEL, max_tokens=1024) 
+llm = OpenAI(model_name=MODEL, max_tokens=1024)
 conversation = ConversationChain(
     llm=llm, verbose=False, memory=ConversationBufferMemory()
-    )
+)
 
-#ルーティング
+
+# ルーティング
 @app.route("/")
 def index():
     return render_template("form.html", title="入力画面")
@@ -24,23 +25,19 @@ def index():
 def write():
     style = request.form["style"]
     sentence = request.form["sentence"]
-    
+
     # styleの記憶
     _ = conversation("【】で囲まれた文章の文体は文体Aです。" + "【" + style + "】")
-    
+
     # 返信文生成
-    response=conversation(
-        "【】で囲まれた文章に対して、文体Aで返信文を作成してください。" \
-         + "【" + sentence + "】"
-         )
+    response = conversation("【】で囲まれた文章に対して、文体Aで返信文を作成してください。" + "【" + sentence + "】")
 
     # 履歴削除
     conversation.memory.clear()
-    
-    return render_template(
-        "result.html", title="結果画面", result=response['response']
-        )
 
-#おまじない
+    return render_template("result.html", title="結果画面", result=response["response"])
+
+
+# おまじない
 if __name__ == "__main__":
-    app.run(port=8080, host='0.0.0.0') #host='0.0.0.0'で外部公開を行う
+    app.run(port=8080, host="0.0.0.0")  # host='0.0.0.0'で外部公開を行う
